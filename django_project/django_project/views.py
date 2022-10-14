3caf82fb679717cde9ab7b896243362408135f7f
zeeshan <zeeshantalking@gmail.com>
2022-10-14 23:55:48 +0500

    Debug = false

--- django_project/django_project/views.py
+++ django_project/django_project/views.py
@@ -2,4 +2,4 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index of django website.")
+    return HttpResponse("<h1>Hello, world. You're at the polls index of django website.</h1>")
