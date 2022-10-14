463e22606cead1dcb1d69605605c751ab9aa5332
zeeshan <zeeshantalking@gmail.com>
2022-10-14 23:27:26 +0500

    ajnago project

--- django_project/django_project/settings.py
+++ django_project/django_project/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 #DEBUG = True
 DEBUG = os.getenv('DEBUG')
 
-ALLOWED_HOSTS = ['zeeshan2.techenablers.info']
+ALLOWED_HOSTS = ['zeeshan2.techenablers.info', 'django']
 
 
 # Application definition
