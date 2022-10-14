b339aa0091cbffb02c6fbcc348f19063ec6c74f4
zeeshan <zeeshantalking@gmail.com>
2022-10-15 00:04:07 +0500

    home view back

--- django_project/django_project/settings.py
+++ django_project/django_project/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 #DEBUG = True
-DEBUG = os.getenv('DEBUG')
+DEBUG = False
 
 ALLOWED_HOSTS = ['zeeshan2.techenablers.info', 'django']
 
